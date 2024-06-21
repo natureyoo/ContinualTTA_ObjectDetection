30fb4ea1c59507032d5c329a50f169e39497b972
nature <jayeon.yoo@snu.ac.kr>
2024-06-21 03:00:22 -0700

    fix

--- scripts/coco_adapt.sh
+++ scripts/coco_adapt.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Backbone Type
+export bb="R50" #"swinT"
+
+# Set config file
+export config_path="configs/TTA/COCO_${bb}.yaml"
+
 # swinT
 #export config_path1="./configs/COCO-Detection/faster_rcnn_swinT_FPN_1x.yaml"
 #export config_path2="./configs/TTA/COCO_swinT.yaml"
@@ -13,32 +19,29 @@ export feature_path="./models/faster_rcnn_r50_coco_feature_stats.pt"
 
 # Direct Test
 python tools/train_net.py \
-  	--config-file ${config_path1} \
+  	--config-file "./configs/COCO-Detection/faster_rcnn_${bb}_FPN_1x.yaml" \
   	--eval-only --wandb \
-  	MODEL.WEIGHTS ${output_dir}/model_final.pth \
-  	SOLVER.IMS_PER_BATCH_TEST 16 \
   	TEST.CONTINUAL_DOMAIN "True" \
-	  OUTPUT_DIR ${output_dir}_direct_test
+	OUTPUT_DIR outputs/COCO/${bb}_direct_test
 
 # full finetuning
 export where="full"
 for continual in False True
 do python tools/train_net.py \
-  	--config-file ${config_path2} \
+  	--config-file ${config_path} \
   	--eval-only --wandb \
   	TEST.ADAPTATION.WHERE ${where} \
   	TEST.ADAPTATION.CONTINUAL ${continual} \
-	  OUTPUT_DIR ${output_dir}_${where}_continual_${continual}
+	OUTPUT_DIR outputs/COCO/${bb}_full_finetuning_continual_${continual}
 done
 
-# adapter tuning
+# ours
 export where="adapter"
 for continual in False True
 do python tools/train_net.py \
-  	--config-file ${config_path2} \
+  	--config-file ${config_path} \
   	--eval-only --wandb \
-  	SOLVER.BASE_LR 0.001 \
   	TEST.ADAPTATION.WHERE ${where} \
   	TEST.ADAPTATION.CONTINUAL ${continual} \
-	  OUTPUT_DIR ${output_dir}_${where}_continual_${continual}
+	OUTPUT_DIR outputs/COCO/${bb}_ours_continual_${continual}
 done
