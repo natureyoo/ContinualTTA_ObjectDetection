aba5be08c7dd745558a265f1e63cbda228ab353a
nature <jayeon.yoo@snu.ac.kr>
2024-06-21 02:51:03 -0700

    remove unnecessary

--- scripts/shift_continuous_resnet50.sh
+++ scripts/shift_continuous_resnet50.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
-for lr in 0.001 0.005
-do for shift_type in "clear_to_foggy" "clear_to_rainy"
-	do for dir_name in "continuous100x"
+# Backbone Type
+export bb="R50" #"swinT"
+
+# Set config file
+export config_path="configs/TTA/SHIFT_continuous_${bb}.yaml"
+
+# CTA via ours
+for shift_type in "clear_to_foggy" "clear_to_rainy"
+	do for dir_name in "continuous100x" "continuous10x"
 		do python tools/train_net.py \
-		--config-file configs/TTA/SHIFT_R50.yaml --eval-only --wandb \
+		--config-file configs/TTA/SHIFT_continuous_R50.yaml --eval-only --wandb \
 		DATASETS.SHIFT.SHIFT_TYPE ${shift_type} \
 		DATASETS.SHIFT.DIRNAME ${dir_name} \
-		SOLVER.BASE_LR ${lr} \
-		SOLVER.CLIP_GRADIENTS.CLIP_VALUE 5.0 \
 		TEST.ONLINE_ADAPTATION True \
 		TEST.ADAPTATION.WHERE adapter \
 		TEST.ADAPTER.TYPE parallel \
-		OUTPUT_DIR outputs/SHIFT_${dir_name}_${shift_type}/R50_adapter_lr${lr}_clip5.0
+		OUTPUT_DIR outputs/SHIFT_${dir_name}_${shift_type}/${bb}_ours
         done
 	done
-done
