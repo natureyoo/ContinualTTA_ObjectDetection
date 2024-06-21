0aa5307a17f849a666a9f4491d8f1d6d716e6da4
nature <jayeon.yoo@snu.ac.kr>
2024-06-21 02:53:57 -0700

    remove unnecessary

--- scripts/shift_continuous_adapt.sh
+++ scripts/shift_continuous_adapt.sh
@@ -10,7 +10,7 @@ export config_path="configs/TTA/SHIFT_continuous_${bb}.yaml"
 for shift_type in "clear_to_foggy" "clear_to_rainy"
 	do for dir_name in "continuous100x" "continuous10x"
 		do python tools/train_net.py \
-		--config-file configs/TTA/SHIFT_continuous_R50.yaml --eval-only --wandb \
+		--config-file configs/TTA/SHIFT_continuous_${bb}.yaml --eval-only --wandb \
 		DATASETS.SHIFT.SHIFT_TYPE ${shift_type} \
 		DATASETS.SHIFT.DIRNAME ${dir_name} \
 		TEST.ONLINE_ADAPTATION True \
