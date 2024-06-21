fabeee175963bac37d7fabb70b0f970f3cf3ffaf
nature <jayeon.yoo@snu.ac.kr>
2024-06-21 03:01:37 -0700

    fix

--- scripts/coco_adapt.sh
+++ scripts/coco_adapt.sh
@@ -6,17 +6,6 @@ export bb="R50" #"swinT"
 # Set config file
 export config_path="configs/TTA/COCO_${bb}.yaml"
 
-# swinT
-#export config_path1="./configs/COCO-Detection/faster_rcnn_swinT_FPN_1x.yaml"
-#export config_path2="./configs/TTA/COCO_swinT.yaml"
-#export output_dir="./outputs/COCO/faster_rcnn_swinT"
-#export feature_path="./models/faster_rcnn_swinT_coco_feature_stats.pt"
-# resne50
-export config_path1="./configs/COCO-Detection/faster_rcnn_R_50_FPN_1x.yaml"
-export config_path2="./configs/TTA/COCO_resnet50.yaml"
-export output_dir="./outputs/COCO/faster_rcnn_resnet50"
-export feature_path="./models/faster_rcnn_r50_coco_feature_stats.pt"
-
 # Direct Test
 python tools/train_net.py \
   	--config-file "./configs/COCO-Detection/faster_rcnn_${bb}_FPN_1x.yaml" \
